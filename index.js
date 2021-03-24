429915a8ec853f378b7e96cab88a6596e937d015
Jhoncito <jhon.electronica@gmail.com>
2021-03-24 10:37:09 -0500

    UInner HTML por medio del botón add

--- index.js
+++ index.js
@@ -15,8 +15,24 @@ document.addEventListener('DOMContentLoaded', function() {
       return
     } //else {
       //console.log('ok')
-
-    //}
+      //}
+    alert.classList.add('d-none')
+    const row = table.insertRow()
+    row.innerHTML = `
+      <td>${title.value}</td>
+      <td>${description.value}</td>
+      <td class="text-center">
+        <input type="checkbox">
+      </td>
+      <td class="text-right">
+        <button class="btn btn-primary mb-1">
+          <i class="fa fa-pencil"></i>
+        </button>
+        <button class="btn btn-danger mb-1 ml-1">
+          <i class="fa fa-trash"></i>
+        </button>
+      </td>
+    `
   }
 })
 
